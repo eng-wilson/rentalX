3899fc8be71b849c08b4c39b4db425375ad0d189
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-07-29 22:36:25 -0300

    feat: add create and list specifications routes

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,31 @@
+import { Specification } from '../model/Specification';
+import {
+  ISpecificationsRepository,
+  ICreateSpecificationDTO,
+} from './ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+
+    return specification;
+  }
+  list(): Specification[] {
+    return this.specifications;
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification({ name, description });
+
+    this.specifications.push(specification);
+  }
+}
+
+export { SpecificationsRepository };
